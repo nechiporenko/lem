84cce21173da57c57b24b5c797404144af023cd6
nechiporenko <s.nechiporenko@gmail.com>
2016-04-05 18:47:36 +0300

    Day commit

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,9 @@
 
 // Десктоп меню (выпадайки)
 // Мобильное меню
-// Сообщения об отправке формы
-// Кнопка скролла страницы
+// Стилизуем хидер при скролле
+// Форма поиска
+// Маска для телефонного номера
 // Модальное окно
 // Слайдер
 
@@ -20,7 +21,9 @@ jQuery(document).ready(function ($) {
     // Десктоп меню (выпадайки)
     //---------------------------------------------------------------------------------------
     (function () {
-        $('.js-menu li').on({
+        var $menu = $('.js-menu li');
+        $menu.has('ul').children('a').append('&nbsp;<i class="icomoon-dropdown"></i>').addClass('has-menu');
+        $menu.on({
             mouseenter: function () {
                 $(this).find('ul:first').stop(true, true).fadeIn('fast');
                 $(this).find('a:first').addClass('hover');
@@ -30,16 +33,118 @@ jQuery(document).ready(function ($) {
                 $(this).find('a:first').removeClass('hover');
             }
         });
+        $menu.on('click', '.has-menu', function (e) {//запретим клик по заголовку под-меню
+            e.preventDefault();
+        });
     })();
 
     //
     // Мобильное меню
     //---------------------------------------------------------------------------------------
     (function () {
+        var $menu = $('.js-mm'),
+            $toggle_btn = $('.js-mm-toggle'),
+            $menu_btn = $menu.find('li').has('ul').addClass('has-menu').children('a'),
+            $submenu = $menu.find('.has-menu').children('ul'),
+            method = {};
+
+
+        $toggle_btn.on('click', function () {
+            if ($(this).hasClass('active')) {
+                method.close();
+            } else {
+                method.show();
+            }
+        });
+
+        $menu.on('click', '.m-menu__label', function () {
+            method.close();
+        });
+
+        $menu.on('click', '.has-menu > a', function (e) {
+            e.preventDefault();
+            if ($(this).hasClass('active')) {
+                method.closeSubMenu();
+            } else {
+                method.closeSubMenu();
+                method.showSubMenu($(this));
+            }
+        });
+
+        method.show = function () {
+            $html.css('overflow', 'hidden');
+            $overlay.show().bind('click', method.close);
+            $toggle_btn.addClass('active');
+            $menu.addClass('active');
+        };
+
+        method.close = function () {
+            $overlay.hide().unbind('click', method.close);
+            method.closeSubMenu();
+            $toggle_btn.removeClass('active');
+            $menu.removeClass('active');
+            $html.css('overflow', 'auto');
+        };
+
+        method.showSubMenu = function (el) {
+            method.closeSubMenu();
+            el.addClass('active').parent('li').children('ul').slideDown();
+        };
+
+        method.closeSubMenu = function () {
+            $submenu.slideUp();
+            $menu_btn.removeClass('active');
+        };
 
     })();
 
+    //
+    // Стилизуем хидер при скролле
+    //---------------------------------------------------------------------------------------
+    (function () {
+        var $header = $('.b-header'),
+            isStick = false, //флаг
+            checkScroll;
 
+        (checkScroll = function () {
+            var fromTop = $window.scrollTop();
+            if (fromTop > 0 && !isStick) {
+                $header.addClass('scrolled');
+                isStick = true;
+            };
+            if (fromTop === 0 && isStick) {
+                $header.removeClass('scrolled');
+                isStick = false;
+            }
+        })();
+
+        $window.bind('scroll', checkScroll);
+    })();
+
+    //
+    // Форма поиска
+    //---------------------------------------------------------------------------------------
+    (function () {
+        var $search__btn = $('[data-searchform]'),
+            $form = $('.b-search'),
+            method = {};
+
+        method.show = function () {
+            $form.fadeIn(400);
+        };
+
+        method.hide = function () {
+            $form.hide();
+        };
+
+        $('.b-header').on('click', '[data-searchform]', method.show);
+        $form.on('click', '.b-search__close', method.hide);
+    })();
+
+    //
+    // Маска для телефонного номера
+    //---------------------------------------------------------------------------------------
+    $('.js-phone-input').mask('+7 (999) 999 - 99 - 99');
 
     //
     // Модальное окно
@@ -50,11 +155,10 @@ jQuery(document).ready(function ($) {
         $modal,
         $close;
 
-        $close = $('<a class="modal__close" href="#"><i class="icon-cancel"></i></a>'); //иконка закрыть
+        $close = $('<button type="button" class="modal__close"><i class="icomoon-cross"></i></button>'); //иконка закрыть
 
 
-        $close.on('click', function (e) {
-            e.preventDefault();
+        $close.on('click', function () {
             method.close();
         });
 
@@ -77,8 +181,8 @@ jQuery(document).ready(function ($) {
             $modal = $(link);
             $modal.append($close);
             method.center();
-            $window.bind('resize.modal', method.center);
-            $modal.show();
+            $window.bind('resize.modal', method.center).trigger('resize');
+            $modal.fadeIn(400);
             $overlay.show().bind('click', method.close);
         };
 
@@ -111,7 +215,8 @@ jQuery(document).ready(function ($) {
             nextText: '<i class="icomoon-right"></i>',
             prevText: '<i class="icomoon-left"></i>',
             auto: true,
-            pause: 8000
+            pause: 8000,
+            autoHover:true
         });
     }
     if ($('.js-slider').length) { initSlider();}
